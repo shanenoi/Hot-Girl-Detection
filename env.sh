b16295ed23cc6e5798f7fce3406fe5ec130854b3
shanenoi <shanenoi.org@gmail.com>
2020-07-15 23:39:15 +0700

    i think this is the last commit :(

--- env.sh
+++ env.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/bash
 
-
+#       ____________________________       #
 # =====[ DEFINE IMPORTANT VARIABLES ]===== #
 export CURRENT_WORKSPACE=$(pwd)
 export PHANTOMJS_DRIVER=$HOME/Driver/phantomjs
-export DATA=$CURRENT_WORKSPACE/data/
-export CONFIG=$CURRENT_WORKSPACE/config/
+export DATA=$CURRENT_WORKSPACE/data
+export CONFIG=$CURRENT_WORKSPACE/config
 
-
+#       ________________                   #
 # =====[ DOWNLOAD LINKS ]================= #
 phantomjs="https://cdn.glitch.com/c16f71ef-e58b-4291-89f3-62975c2ebc7f%2Fphantomjs?v=1594230459325"
 yolo_config="https://cdn.glitch.com/c16f71ef-e58b-4291-89f3-62975c2ebc7f%2Fyolov3-face.cfg?v=1586632897107"
 yolo_weights="https://cdn.glitch.com/c16f71ef-e58b-4291-89f3-62975c2ebc7f%2Fyolov3-wider_16000.weights?v=1586632996143"
 
-
+#       _______________________            #
 # =====[ DEFINE SOME FUNCTIONS ]========== #
 count_files () {
     local folder=$1
@@ -24,7 +24,7 @@ count_files () {
     echo $sum
 }
 
-
+#       ________________________           #
 # =====[ CHECK PHANTOMJS DRIVER ]========= #
 if [ -f "$PHANTOMJS_DRIVER" ]; then
     echo "[✓] PhantomJs was installed."
@@ -36,7 +36,8 @@ else
     echo "[✓] saved to $PHANTOMJS_DRIVER."
 fi
 
-
+chmod +x $PHANTOMJS_DRIVER
+#       ___________________                #
 # =====[ CHECK DATA FOLDER ]============== #
 if [ -d "$DATA" ]; then
     echo "[✓] data folder existed."
@@ -46,7 +47,7 @@ else
     echo "[✓] created data folder."
 fi
 
-
+#       _________________________          #
 # =====[ CHECK YOLO CONFIG FILES ]======== #
 if [ $(count_files $CONFIG) -ne 0 ]; then
     echo "[✓] config files were downloaded!"
